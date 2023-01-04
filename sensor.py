0adeaf0571d33ee743127e97a8b2f8032acd5b88
Daan Pape <daan@dptechnics.com>
2023-01-04 23:05:13 +0000

    The AGSO accumulated water value is visible in Home Assistant

--- sensor.py
+++ sensor.py
@@ -7,14 +7,15 @@ import logging
 from .agso_cloud import AgsoCloud
 
 from homeassistant.const import UnitOfVolume
-from homeassistant.const import CONF_NAME, CONF_PASSWORD, CONF_URL, CONF_USERNAME
+from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.components.sensor import SensorStateClass
 from homeassistant.components.sensor import SensorDeviceClass
+from homeassistant.components.sensor import SensorEntityDescription
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.util import Throttle, dt
+from homeassistant.util import Throttle
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -38,13 +39,23 @@ async def async_setup_entry(
     agso = AgsoCloud(username, password)
     subscribers = await hass.async_add_executor_job(get_subscribers, agso)
 
+    uniqueId = (
+        str(subscribers[0].customer_number)
+        + "-"
+        + subscribers[0].subscriber_number
+        + "-"
+        + str(subscribers[0].meters[0].meter_number)
+    )
+
     # Todo create probe for every water meter
-    probe = AgsoMeterProbe(agso, "AccumulatedValue", "", "", "")
-    entities = [
-        AgsoAccumulatedConsumption(
-            probe, "AgsoAccumulatedConsumption", "uniqueidtest", "accumulated"
-        )
-    ]
+    probe = AgsoMeterProbe(
+        agso,
+        "AccumulatedValue",
+        subscribers[0].customer_number,
+        subscribers[0].subscriber_number,
+        subscribers[0].meters[0].meter_number,
+    )
+    entities = [AgsoAccumulatedConsumption(probe, uniqueId)]
 
     async_add_entities(entities, True)
 
@@ -68,29 +79,33 @@ class AgsoMeterProbe:
     def update(self):
         """Update probe data."""
 
+        _LOGGER.info("Fetching AGSO data")
+
         try:
             if self.data_type == "AccumulatedValue":
                 self.value = self.agso.get_current_meter_reading().value
+                _LOGGER.info(
+                    "Got accumulated value: "
+                    + str(self.agso.get_current_meter_reading().value)
+                )
 
         except:
             _LOGGER.error("Unable to get " + self.data_type + " from the AGSO cloud")
 
 
 class AgsoAccumulatedConsumption(SensorEntity):
-    def __init__(self, probe, name, unique_id, description):
+    def __init__(self, probe, unique_id):
         self.probe = probe
         self._attr_name = "Accumulated Water Consumption"
+        self._attr_unique_id = unique_id + "-accumulated-consumption"
         self._attr_native_unit_of_measurement = UnitOfVolume.LITERS
         self._attr_device_class = SensorDeviceClass.WATER
         self._attr_state_class = SensorStateClass.TOTAL_INCREASING
 
-        @property
-        def native_value(self):
-            """Return the state of the sensor."""
-            result = self.probe.value
-            if self.entity_description.precision is not None:
-                result = round(result, self.entity_description.precision)
-            return result
+    @property
+    def native_value(self):
+        """Return the state of the sensor."""
+        return self.probe.value
 
-        def update(self) -> None:
-            self.probe.update()
+    def update(self) -> None:
+        self.probe.update()
